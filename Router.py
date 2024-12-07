724c3bcd2034d0c55ab9df49efdb07fc83b25e99
AL-DN <aldensahi50@gmail.com>
2024-12-07 13:49:34 -0500

    first commit

---
+++ Router.py
@@ -0,0 +1,21 @@
+# Author: Alden Sahi
+# Date: 12/06/2024
+# Project Description: to determine the average wait time for packets, the number of
+# packets that are dropped by the router, and the overall utilization of the router.
+
+
+from packetQueue import packetQueue
+
+queue = packetQueue()
+queue.enqueue(1)
+queue.enqueue(1)
+queue.enqueue(1)
+
+for i in range(queue.count()):
+    print(queue.dequeue())
+    
+
+    
+
+
+
---
+++ packetQueue.py
@@ -0,0 +1,18 @@
+# Author: Alden Sahi
+
+class packetQueue:
+    def __init__(self):
+        # packet queue
+        self.packets = []
+
+    # returns # packets 
+    def count(self):
+        return len(self.packets)
+    
+    # adds packet to end of queue
+    def enqueue(self, packet):
+        self.packets.append(packet)
+
+    # removes and returns packet
+    def dequeue(self):
+        return self.packets.pop(0)
